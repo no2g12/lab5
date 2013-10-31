e9c3f9fd512a5bed667d8de4bed1800797e3374f
no2g12 <no2g12@soton.ac.uk>
2013-10-31 14:21:49 +0000

    created a car class

--- classes/car.class.php
+++ classes/car.class.php
@@ -1,3 +1,23 @@
 <?php
-
+class car{
+  public $id;
+  public $manufacturer_id;
+  public $colour_id;
+  public $model;
+  public $description;
+  public $price;
+  public $regNumber;
+  public $regDate;
+  
+  public function __construct($id, $manufacturer_id, $colour_id,$model, $description, $price, $regNumber, $regDate){
+    $this->id = $id;
+    $this->manufacturer_id = $manufacturer_id;
+    $this->colour_id = $colour_id;
+    $this->model = $model;
+    $this->description = $description;
+    $this->price = $price;
+    $this->regNumber = $regNumber;
+    $this->regDate = $regDate;
+  }
+?>
 // car class for part 1
